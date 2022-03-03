96e5eca3c73c4dafb8d9ecc73ddda66b89aee844
Austin A <austinandrews89@gmail.com>
2022-03-03 16:58:41 -0700

    Added choice array and question array

--- index.js
+++ index.js
@@ -3,15 +3,54 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
+const licenseChoices = ["GNU AGPLv3", "GNU GPLv3", "Mozilla Public License 2.0", "MIT", "The Unlicense"]
 const questions = [
     {
         type: 'input',
-        name: ''
+        name: 'description',
+        message: 'Enter a description for the project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter the installation instructions.'
+    },
+    {
+        type: 'input',
+        name: 'usageInfo',
+        message: 'Enter usage information.'
+    },
+    {
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: 'Enter contribution guidelines.'
+    },
+    {
+        type: 'input',
+        name: 'testInstructions',
+        message: 'Enter test instructions.'
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'Enter your GitHub username.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your developer email address.'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for this project.',
+        choices: licenseChoices
     }
-
-
-
-
 ];
 
 // TODO: Create a function to write README file
